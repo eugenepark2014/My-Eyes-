7eca897953225543766fc05f1b2428ddbee83634
eugenepark2014 <eugenepark2014@gmail.com>
2018-04-30 12:31:39 -0400

    TIM!

--- content.js
+++ content.js
@@ -3,10 +3,9 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(request, sender, sendResponse) {
   //Checks to see if the allPage button was pressed
   if (request.allPage) {
-    var elements = document.getElementsByTagName("*");
+    var elements = document.getElementsByTagName("p");
     for (var i = 0; i < elements.length; i++) {
-      elements[i].style.backgroundColor = request.color[0];
-      elements[i].style.color = request.color[1];
+      elements[i].innerHTML = 'TIM!TIM!TIM!TIM!'
     }
   }
   else //otherwise, we know that the start button was used pressed instead
--- popup/popup.js
+++ popup/popup.js
@@ -2,38 +2,33 @@ var slider = document.getElementById("Range");
 var colorValue = document.getElementById("color");
 var fullPage = document.getElementById("fullPage");
 var start = document.getElementById("Start");
-
+//set variables to html elements
 const colors = [
   ["#ffffe6", "#000000", "Paperback"],
   ["#8C0000", "#DFFFF7", "F.Lux"],
   ["#000000", "#DFFFF7", "Pitch Black"]
 ];
-
+//array of theme profiles
 let currentColor = colors[1];
 
 colorValue.innerHTML = currentColor[2]; // Display the default slider value
-
-colorValue.style.color = currentColor[0];
 
 let params = {
   active: true,
   currentWindow: true
 };
-
-
+//this changes the slider name and current color variable
 slider.oninput = function() {
   currentColor = colors[this.value - 1];
-  colorValue.style.color = currentColor[0];
   colorValue.innerHTML = currentColor[2];
 };
-
+//sends a message to the active tab with information
 start.onclick = function() {
   chrome.tabs.query(params, gotTab);
   function gotTab(tab) {
     chrome.tabs.sendMessage(tab[0].id, {
       color: currentColor,
-      allPage: false,
-      check: true
+      allPage: false
     });
   }
 };
@@ -43,8 +38,7 @@ fullPage.onclick = function() {
   function gotTab(tab) {
     chrome.tabs.sendMessage(tab[0].id, {
       color: currentColor,
-      allPage: true,
-      check: false
+      allPage: true
     });
   }
 };
